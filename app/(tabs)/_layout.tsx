7a8e0cfc67466acbee1a2b0e1c7d46a932eec67f
pranavkannepalli <pranav.kannepalli@gmail.com>
2025-03-24 22:54:16 -0700

    feat: add component and home tab

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,17 @@
+import { IconSymbol } from '@/components/ui/IconSymbol';
+import { Tabs } from 'expo-router';
+import React from 'react';
+
+export default function TabLayout() {
+    return (
+        <Tabs
+            screenOptions={{
+                headerShown: false,
+            }}>
+            <Tabs.Screen
+                name="index"
+                options={{ title: "Home", tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} /> }}
+            />
+        </Tabs>
+    )
+}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,9 @@
-import { Stack } from "expo-router";
+import { SplashScreen, Stack } from "expo-router";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  return <Stack />;
+  return <Stack>
+    <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+  </Stack>;
 }
